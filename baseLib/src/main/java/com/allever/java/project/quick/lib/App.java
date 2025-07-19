a02c2eea9de6c061d3cd9eeccaddefbeacb96015
devallever <devallever@163.com>
2025-07-19 11:46:20 +0800

    NONE-CODE: 移除compose配置，添加baseLib模块

---
+++ baseLib/src/main/java/com/allever/java/project/quick/lib/App.java
@@ -0,0 +1,17 @@
+package com.allever.java.project.quick.lib;
+
+import android.app.Application;
+import android.content.Context;
+
+public class App extends Application {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = this;
+    }
+
+
+}
